64da76eed8d7196caf118a7d8ed7f0687afef556
Robert <robert@nash.io>
2021-09-14 20:29:12 +0200

    mpc-wallet-elixir: update rustler to 0.22

--- mpc-wallet/mpc-wallet-elixir/src/lib.rs
+++ mpc-wallet/mpc-wallet-elixir/src/lib.rs
@@ -2,9 +2,6 @@
  * Elixir NIFs to MPC-based API keys
  */
 
-#[macro_use]
-extern crate rustler;
-
 #[cfg(feature = "secp256k1")]
 use nash_mpc::curves::secp256_k1::{Secp256k1Point, Secp256k1Scalar};
 #[cfg(feature = "k256")]
@@ -16,40 +13,26 @@ use nash_mpc::rust_bigint::BigInt;
 use nash_mpc::{client, common, server};
 use rustler::{Encoder, Env, Error, Term};
 
-rustler_export_nifs! {
+rustler::init!(
     "Elixir.Server.MPCwallet",
-    [
-    ("generate_paillier_keypair_and_proof", 0, generate_paillier_keypair_and_proof),
-    ("dh_rpool", 2, dh_rpool),
-    ("complete_sig", 7, complete_sig),
-    ("verify", 5, verify),
-    ("compute_presig", 3, compute_presig),
-    ("fill_rpool", 4, fill_rpool),
-    ("dh_init", 2, dh_init),
-    ("init_api_childkey_creator", 1, init_api_childkey_creator),
-    ("init_api_childkey_creator_with_verified_paillier", 2, init_api_childkey_creator_with_verified_paillier),
-    ("verify_paillier", 3, verify_paillier),
-    ("create_api_childkey", 2, create_api_childkey),
-    ("publickey_from_secretkey", 2, publickey_from_secretkey),
-    ],
-    None
-}
+    [generate_paillier_keypair_and_proof, dh_rpool, complete_sig, verify, compute_presig, fill_rpool, dh_init, init_api_childkey_creator, init_api_childkey_creator_with_verified_paillier, verify_paillier, create_api_childkey, publickey_from_secretkey]
+);
 
 mod atoms {
-    rustler_atoms! {
-        atom ok;
-        atom error;
-        atom __true__ = "true";
-        atom __false__ = "false";
+    rustler::atoms! {
+        ok,
+        error,
+        __true__ = "true",
+        __false__ = "false",
     }
 }
 
 /// generate paillier keypair
 /// input: none
 /// output: paillier secret key, paillier public key, proof that paillier key was generated correctly
+#[rustler::nif]
 fn generate_paillier_keypair_and_proof<'a>(
     env: Env<'a>,
-    _args: &[Term<'a>],
 ) -> Result<Term<'a>, Error> {
     let (paillier_pk, paillier_sk) = server::generate_paillier_keypair();
     let correct_key_proof = server::generate_paillier_proof(&paillier_sk);
@@ -68,15 +51,8 @@ fn generate_paillier_keypair_and_proof<'a>(
 /// Diffie-Hellman, compute random and nonce values to be added to the pool as well as a set of public values.
 /// input: client_dh_publics, curve (Secp256r1 or Secp256k1)
 /// output: rpool_new, server_dh_publics
-fn dh_rpool<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let client_dh_publics_str: String = match args[0].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing client_dh_publics").encode(env)),
-    };
-    let curve_str: String = match args[1].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing curve").encode(env)),
-    };
+#[rustler::nif]
+fn dh_rpool<'a>(env: Env<'a>, client_dh_publics_str: String, curve_str: String) -> Result<Term<'a>, Error> {
     let curve: common::Curve = match serde_json::from_str(&curve_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing curve").encode(env)),
@@ -149,54 +125,27 @@ fn dh_rpool<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
 /// finalize presignature to normal ECDSA signature
 /// input: paillier_sk, presig, r, k, curve, pubkey, msg_hash
 /// output: r, s, recid
-fn complete_sig<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let paillier_sk_str: String = match args[0].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing paillier_sk").encode(env)),
-    };
+#[rustler::nif]
+fn complete_sig<'a>(env: Env<'a>, paillier_sk_str: String, presig_str: String, r_str: String, k_str: String, curve_str: String, pubkey: String , msg_hash_str: String) -> Result<Term<'a>, Error> {
     let paillier_sk: DecryptionKey = match serde_json::from_str(&paillier_sk_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing paillier_sk").encode(env)),
-    };
-    let presig_str: String = match args[1].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing presig").encode(env)),
     };
     let presig = match BigInt::from_hex(&presig_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing presig").encode(env)),
     };
-    let r_str: String = match args[2].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing r").encode(env)),
-    };
     let r = match BigInt::from_hex(&r_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing r").encode(env)),
-    };
-    let k_str: String = match args[3].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing k").encode(env)),
     };
     let k = match BigInt::from_hex(&k_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing k").encode(env)),
     };
-    let curve_str: String = match args[4].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing curve").encode(env)),
-    };
     let curve: common::Curve = match serde_json::from_str(&curve_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing curve").encode(env)),
-    };
-    let pubkey: String = match args[5].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing pubkey").encode(env)),
-    };
-    let msg_hash_str: String = match args[6].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing msg_hash").encode(env)),
     };
     let msg_hash = match BigInt::from_hex(&msg_hash_str) {
         Ok(v) => v,
@@ -223,38 +172,19 @@ fn complete_sig<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> 
 /// verify signature for a message under given public key
 /// input: r, s, pubkey, msg_hash, curve
 /// output: ok|error
-fn verify<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let r_str: String = match args[0].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing r").encode(env)),
-    };
+#[rustler::nif]
+fn verify<'a>(env: Env<'a>, r_str: String, s_str: String, pubkey: String, msg_hash_str: String, curve_str: String) -> Result<Term<'a>, Error> {
     let r = match BigInt::from_hex(&r_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing r").encode(env)),
-    };
-    let s_str: String = match args[1].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing s").encode(env)),
     };
     let s = match BigInt::from_hex(&s_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing s").encode(env)),
     };
-    let pubkey: String = match args[2].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing pubkey").encode(env)),
-    };
-    let msg_hash_str: String = match args[3].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing msg_hash").encode(env)),
-    };
     let msg_hash = match BigInt::from_hex(&msg_hash_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing msg_hash").encode(env)),
-    };
-    let curve_str: String = match args[4].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing curve").encode(env)),
     };
     let curve: common::Curve = match serde_json::from_str(&curve_str) {
         Ok(v) => v,
@@ -271,15 +201,8 @@ fn verify<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
 /// Diffie-Hellman: create a set of secret values and a set of public values.
 /// input: n (number of key pairs to generate), curve (Secp256r1 or Secp256k1)
 /// output: dh_secrets, dh_publics
-fn dh_init<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let n: usize = match args[0].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing n").encode(env)),
-    };
-    let curve_str: String = match args[1].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing curve").encode(env)),
-    };
+#[rustler::nif]
+fn dh_init<'a>(env: Env<'a>, n: usize, curve_str: String) -> Result<Term<'a>, Error> {
     let curve: common::Curve = match serde_json::from_str(&curve_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing curve").encode(env)),
@@ -310,26 +233,15 @@ fn dh_init<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
 /// compute presignature
 /// input: api_childkey, message_hash, curve
 /// output: presignature, r
-fn compute_presig<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let api_childkey_str: String = match args[0].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing api_childkey").encode(env)),
-    };
+#[rustler::nif]
+fn compute_presig<'a>(env: Env<'a>, api_childkey_str: String, msg_hash_str: String, curve_str: String) -> Result<Term<'a>, Error> {
     let api_childkey: client::APIchildkey = match serde_json::from_str(&api_childkey_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing api_childkey").encode(env)),
     };
-    let msg_hash_str: String = match args[1].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing msg_hash").encode(env)),
-    };
     let msg_hash = match BigInt::from_hex(&msg_hash_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing msg_hash").encode(env)),
-    };
-    let curve_str: String = match args[2].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing curve").encode(env)),
     };
     let curve: common::Curve = match serde_json::from_str(&curve_str) {
         Ok(v) => v,
@@ -352,26 +264,11 @@ fn compute_presig<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error
 /// fill pool of r-values from dh secret and public values
 /// input: server_dh_secrets, client_dh_publics, curve, paillier_pk
 /// output: ok|error
-fn fill_rpool<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let server_dh_secrets_str: String = match args[0].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing server_dh_secrets").encode(env)),
-    };
-    let client_dh_publics_str: String = match args[1].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing client_dh_publics").encode(env)),
-    };
-    let curve_str: String = match args[2].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing curve").encode(env)),
-    };
+#[rustler::nif]
+fn fill_rpool<'a>(env: Env<'a>, server_dh_secrets_str: String, client_dh_publics_str: String, curve_str: String, paillier_pk_str: String) -> Result<Term<'a>, Error> {
     let curve: common::Curve = match serde_json::from_str(&curve_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing curve").encode(env)),
-    };
-    let paillier_pk_str: String = match args[3].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing paillier_pk").encode(env)),
     };
     let paillier_pk: EncryptionKey = match serde_json::from_str(&paillier_pk_str) {
         Ok(v) => v,
@@ -433,11 +330,8 @@ fn fill_rpool<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
 /// initialize API child key creation by setting the full secret key
 /// input: secret_key
 /// output: api_childkey_creator
-fn init_api_childkey_creator<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let secret_key_str: String = match args[0].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing secret_key").encode(env)),
-    };
+#[rustler::nif]
+fn init_api_childkey_creator<'a>(env: Env<'a>, secret_key_str: String) -> Result<Term<'a>, Error> {
     let secret_key = match BigInt::from_hex(&secret_key_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing secret_key").encode(env)),
@@ -451,21 +345,15 @@ fn init_api_childkey_creator<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term
 /// initialize API child key creation by setting the full secret key and the paillier public key, assuming that the paillier public key has been verified before.
 /// input: secret_key, paillier_pk
 /// output: apichildkeycreator
+#[rustler::nif]
 fn init_api_childkey_creator_with_verified_paillier<'a>(
     env: Env<'a>,
-    args: &[Term<'a>],
+    secret_key_str: String,
+    paillier_pk_str: String
 ) -> Result<Term<'a>, Error> {
-    let secret_key_str: String = match args[0].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing secret_key").encode(env)),
-    };
     let secret_key = match BigInt::from_hex(&secret_key_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing secret_key").encode(env)),
-    };
-    let paillier_pk_str: String = match args[1].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing paillier_pk").encode(env)),
     };
     let paillier_pk: EncryptionKey = match serde_json::from_str(&paillier_pk_str) {
         Ok(v) => v,
@@ -481,11 +369,8 @@ fn init_api_childkey_creator_with_verified_paillier<'a>(
 /// verify that the Paillier public key was generated correctly.
 /// input: api_childkey_creator, paillier_pk, correct_key_proof
 /// output: api_childkey_creator
-fn verify_paillier<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let api_childkey_creator_str: String = match args[0].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing api_childkey_creator").encode(env)),
-    };
+#[rustler::nif]
+fn verify_paillier<'a>(env: Env<'a>, api_childkey_creator_str: String, paillier_pk_str: String, correct_key_proof_str: String) -> Result<Term<'a>, Error> {
     let api_childkey_creator: client::APIchildkeyCreator =
         match serde_json::from_str(&api_childkey_creator_str) {
             Ok(v) => v,
@@ -495,17 +380,9 @@ fn verify_paillier<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Erro
                 )
             }
         };
-    let paillier_pk_str: String = match args[1].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing paillier_pk").encode(env)),
-    };
     let paillier_pk: EncryptionKey = match serde_json::from_str(&paillier_pk_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing paillier_pk").encode(env)),
-    };
-    let correct_key_proof_str: String = match args[2].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing correct_key_proof").encode(env)),
     };
     let correct_key_proof: common::CorrectKeyProof =
         match serde_json::from_str(&correct_key_proof_str) {
@@ -527,11 +404,8 @@ fn verify_paillier<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Erro
 /// create API childkey
 /// input: api_childkey_creator, curve
 /// output: api_childkey
-fn create_api_childkey<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let api_childkey_creator_str: String = match args[0].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing api_childkey_creator").encode(env)),
-    };
+#[rustler::nif]
+fn create_api_childkey<'a>(env: Env<'a>, api_childkey_creator_str: String, curve_str: String) -> Result<Term<'a>, Error> {
     let api_childkey_creator: client::APIchildkeyCreator =
         match serde_json::from_str(&api_childkey_creator_str) {
             Ok(v) => v,
@@ -541,10 +415,6 @@ fn create_api_childkey<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, 
                 )
             }
         };
-    let curve_str: String = match args[1].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing curve").encode(env)),
-    };
     let curve: common::Curve = match serde_json::from_str(&curve_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing curve").encode(env)),
@@ -561,18 +431,11 @@ fn create_api_childkey<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, 
 /// Derive public key from given secret key.
 /// input: (full) secret key, curve
 /// output: public_key
-fn publickey_from_secretkey<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let secret_key_str: String = match args[0].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing secret_key").encode(env)),
-    };
+#[rustler::nif]
+fn publickey_from_secretkey<'a>(env: Env<'a>, secret_key_str: String, curve_str: String) -> Result<Term<'a>, Error> {
     let secret_key = match BigInt::from_hex(&secret_key_str) {
         Ok(v) => v,
         Err(_) => return Ok((atoms::error(), &"error deserializing secret_key").encode(env)),
-    };
-    let curve_str: String = match args[1].decode() {
-        Ok(v) => v,
-        Err(_) => return Ok((atoms::error(), &"error parsing curve").encode(env)),
     };
     let curve: common::Curve = match serde_json::from_str(&curve_str) {
         Ok(v) => v,
